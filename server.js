ad575d621beb84da8ec3d4a25645fa384b40a9df
tushar <tushargadher123@gmail.com>
2024-02-24 16:17:00 +0530

    upload

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import pkg from "express";
 const { json, httpServer } = pkg;
-// import chats from "./Data/data";
 import { config } from "dotenv";
 import cors from "cors";
 import { Server } from "socket.io";
-// import
 import connectToMongo from "./config/db.js";
 import userRoutes from "./Routes/userRoutes.js";
 import chatRoutes from "./Routes/chatRoutes.js";
@@ -17,19 +15,15 @@ const FrontEnd = "http://localhost:5173/";
 
 config();
 connectToMongo();
-
 app.use(cors());
 app.use(json()); //to accepet json data
-
 //api routes
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoute);
-
 const server = app.listen(port, () => {
   console.log(`server is running on http://${host}:${port}`);
 });
-
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
@@ -39,14 +33,12 @@ const io = new Server(server, {
 //on means what happen when event is emit
 io.on("connection", (socket) => {
   console.log("connection to socket.io");
-
   //creating new socket
   socket.on("setup", (userData) => {
     //getting data from frontEnd and creating a room for user
     socket.join(userData._id);
     socket.emit("connected");
   });
-
   //joing chat socket,it will take room id from the FrontEnd
   socket.on("join chat", (room) => {
     //create a room with room id
